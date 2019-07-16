8df0ee49cc013644a23bdba0d3756609f7d89318
adlof-hitler <1002930049@qq.com>
2019-07-16 09:19:14 +0800

    准备
    写好基本配置和导入样式

--- src/main.js
+++ src/main.js
@@ -3,6 +3,28 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// 导入路由
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+// 导入全局样式
+import './assets/base.scss'
+
+// 导入组件
+import index from './views/index.vue'
+import login from './views/login.vue'
+
+const router =[
+{path:'/login',component:login},
+{path:'/index',component:index},
+]
+
+const routes=new VueRouter({
+  router
+})
+
 new Vue({
   render: h => h(App),
+  // 挂在到vue实例上
+  routes
 }).$mount('#app')
